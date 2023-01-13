b0d7d46b2df2227bd7518c0c01ac375b5473aed9
Jesus Vasquez <jesusvasquez384@gmail.com>
2023-01-13 00:31:22 -0500

    App info

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,9 +6,11 @@ const App = () => {
     <div className="blankMessage">
       <h1>Blank ReactJS Project</h1>
       <p>This is a simple Blank ReactJS project.</p>
-	  <p><b>What is in this project?</b></p>
-	  <p>React 18 + Webpack + Babel + SASS</p>
-	  <h3>Enjoy it :)</h3>
+      <p>
+        <b>What's in this project?</b>
+      </p>
+      <p>React V.18 + Webpack + Babel + SASS</p>
+      <h3>Enjoy it!</h3>
     </div>
   );
 };
